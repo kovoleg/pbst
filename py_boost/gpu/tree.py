2f9e047e9d77f8f9c98eaaec7fb2735aa2e55b25
kovoleg <114398800+kovoleg@users.noreply.github.com>
2024-03-05 13:33:02 +0300

    Update tree.py

--- py_boost/gpu/tree.py
+++ py_boost/gpu/tree.py
@@ -515,7 +515,8 @@ class DepthwiseTreeBuilder:
             output_groups = [cp.arange(grad.shape[1], dtype=cp.uint64)]
         else: # ---------------------------------------------------------------------------------------------------------------------------------------------------
             # output_groups = self.target_grouper()
-            groups = DBSCAN(eps=22, min_samples=2).fit(cp.transpose(grad).get()).labels_
+            groups = DBSCAN(eps=30, min_samples=2).fit(cp.transpose(grad).get()).labels_
+            print(groups)
             print(pairwise_distances(cp.transpose(grad).get()))
             output_groups = []
             for i in range(len(np.unique(groups))):
