33f753a27e4ad97dd1cc276cc94ba382072b5a26
kovoleg <114398800+kovoleg@users.noreply.github.com>
2024-03-07 22:30:04 +0300

    Update boosting.py

--- py_boost/gpu/boosting.py
+++ py_boost/gpu/boosting.py
@@ -147,8 +147,9 @@ class GradientBoosting(Ensemble):
             splitter = RandomGroupsSplitter(6)
         elif self.params['target_splitter'] == 'Random10':
             splitter = RandomGroupsSplitter(10)
-        else:
-            splitter = self.params['target_splitter']
+        print(self.params['target_splitter'])
+        # else:
+        #     splitter = self.params['target_splitter']
 
         self.target_splitter = splitter
 
