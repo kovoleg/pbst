9a3c087a6d093df64e8bc69ca34184d166c7d970
kovoleg <114398800+kovoleg@users.noreply.github.com>
2024-04-15 14:56:32 +0300

    Update tree.py

--- py_boost/gpu/tree.py
+++ py_boost/gpu/tree.py
@@ -533,7 +533,9 @@ class DepthwiseTreeBuilder:
                 mtx = hess
             else:
                 mtx = grad
-
+                
+            print(cp.shape(mtx))
+            print(cp.shape(cp.transpose(mtx)))
             if self.dim_red == True:
                 # emb = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=50).fit_transform(cp.transpose(mtx).get())
                 emb = PCA(n_components='mle').fit_transform(cp.transpose(mtx).get())
