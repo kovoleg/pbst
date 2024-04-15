6a60adb18e8ecd1c02d9b946dca936d9c62eaca1
kovoleg <114398800+kovoleg@users.noreply.github.com>
2024-04-15 14:41:19 +0300

    Update tree.py

--- py_boost/gpu/tree.py
+++ py_boost/gpu/tree.py
@@ -576,13 +576,14 @@ class DepthwiseTreeBuilder:
             #     real_output.append(cur_group)
                 
             # output_groups = real_output
+            
             print(output_groups)
-            if self.dim_red == True:
-                np.random.seed(seed=7)
-                color = np.random.rand(len(output_groups) + 1, 3)
-                for i in range(len(output_groups)):
-                    plt.scatter(emb[output_groups[i], 0], emb[output_groups[i], 1], c=color[i].reshape(1,-1))
-                plt.show()
+            # if self.dim_red == True:
+            #     np.random.seed(seed=7)
+            #     color = np.random.rand(len(output_groups) + 1, 3)
+            #     for i in range(len(output_groups)):
+            #         plt.scatter(emb[output_groups[i], 0], emb[output_groups[i], 1], c=color[i].reshape(1,-1))
+            #     plt.show()
             else:
                 print(output_groups)
         else:
